459ac744b0ee07460849c387028b9395a1cae310
TGM - Thiago Mota <thiago.mota@totvs.com.br>
2018-10-03 00:46:56 -0300

    Account and Category references

--- src/api/transaction/transaction.js
+++ src/api/transaction/transaction.js
@@ -4,12 +4,12 @@ const mongoose = restful.mongoose
 const Transaction = new mongoose.Schema({
     execution_date : { type : Date, default : Date.now },
     due_date : { type : Date, default : Date.now },
-    description : { type : String, required : true },
-    category_id : {type : String, required : true },
-    debit_account_id : { type : String },
-    credit_account_id : { type : String },
-    value : { type : Number, required : true, default : 0, min : 0 },
-    consolidated : { type: Boolean, required : true, default : true },
+    description : { type : String, require : true },
+    category_id : { type: 'ObjectId', ref: 'Category', require: true},
+    debit_account_id : { type: 'ObjectId', ref: 'Account', require: true },
+    credit_account_id : { type: 'ObjectId', ref: 'Account', require: true },
+    value : { type : Number, require : true, default : 0, min : 0 },
+    consolidated : { type: Boolean, require : true, default : true },
     createdAt : { type : Date, default : Date.now }
 });
 
