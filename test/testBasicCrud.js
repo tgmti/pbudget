900326860312f12d9561d9de00dec1477c9a7414
TGM - Thiago Mota <thiago.mota@totvs.com.br>
2018-10-03 00:47:41 -0300

    POST and PUT tests

--- test/testBasicCrud.js
+++ test/testBasicCrud.js
@@ -3,14 +3,19 @@ let chaiHttp = require('chai-http');
 let should = chai.should();
 let urlbase = 'http://localhost:3005/api';
 let testPaths = [ 
-        { name: 'Account', path: '/account' } ,
-        { name: 'Category', path: '/category' } ,
-        { name: 'Transaction', path: '/transaction' } ,
-    ];
-
+    { name: 'Account', path: '/account', post: {name : 'Carteira', type : 'WALLET'} },
+    { name: 'Category', path: '/category', post: {name : 'Despesas'}},
+    { name: 'Transaction', path: '/transaction', post: {
+        description : 'teste despesa',
+        category_id : '5bb413619993f42f08cd8704', //TODO: buscar id como nos testes?
+        debit_account_id : '5bb412d77dab5a0b1018021f',
+        value : 10,
+    }} ,
+];
+    
 chai.use(chaiHttp);
 
-describe('Get', () => {
+describe('GET', () => {
 
     testPaths.map( (test) => {
         it('/GET ' + test.name, (done) => {
@@ -25,17 +30,47 @@ describe('Get', () => {
     });
 });
 
+describe('POST', () => {
+
+    testPaths.map( (test) => {
+        it('/POST ' + test.name, (done) => {
+            chai.request(urlbase)
+            .post(test.path)
+            .send(test.post)
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.be.a('object');
+                done();
+            });
+        });
+    });
+});
+
+describe('PUT', () => {
+
+    it('/PUT :id Transaction ', (done) => {
+        chai.request(urlbase)
+        .put("/api/transaction/5bb41feea9719f3284c9cd42")
+        .send(testPaths[2].post)
+        .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
 /*
     TODO:
     [X] Configurar banco de teste
-    [ ] Testar inclusão de contas
-    [ ] Testar inclusão de Categorias
-    [ ] Testar inclusão de transações
-    [ ] Testar inclusão de orçamento
+    [X] Testar inclusão de contas
+    [X] Testar inclusão de Categorias
+    [X] Testar inclusão de transações
     [ ] Criar Schema Orçamento
         [ ] Mes/Ano
             [ ] Categorias
                 [ ] Valores Previstos
+    [ ] Testar inclusão de orçamento
     [ ] Testar exclusões e atualizações
     [ ] Testar Get de orçamento, com valores previstos e reais
 
@@ -45,5 +80,9 @@ describe('Get', () => {
         Consolidados ou não
 
     [ ] Precisa de um tipo de conta "Cartão de crédito"? (type 'CREDCARD')
+
+    Populate: Trás a transação com os dados da categoria e das contas
+    http://localhost:3005/api/transaction/5bb42d35a22b071da4969a97?populate=category_id&populate=debit_account_id&populate=credit_account_id
+
 
 */
